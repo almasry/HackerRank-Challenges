619fd5e05fe29c2617efe27d40a40438df782a22
petersparker <rp-github-2k14@ranger-pete.com>
2014-08-18 23:22:50 -0700

    Changes, ready for debugging

--- www.hacker-challenge.dev/Utopian Tree/Utopian Tree.php
+++ www.hacker-challenge.dev/Utopian Tree/Utopian Tree.php
@@ -22,29 +22,34 @@
 //  BUGS: 
 //
 
-function GrowTree($BeginningHeight, $Years) {
-		for ($i=1; $i <=$Years ; $i++) { 
-			$BeginningHeight *= 2;
-			$BeginningHeight += 1;
-		} // End For
+function GrowTree($Cycles) {
+		$TreeHeight = 1;
+		If ($Cycles == 0) return $TreeHeight; //Tree did not grow
+		for ($i=1; $i=$Cycles; $i++) { 
+			If (($Cycles%2) == 1) {
+				$TreeHeight *= 2;
+			} else {
+				$TreeHeight += 1;
+			} // End For
 		return $TreeHeight;
 }
+
 //  Main
 
 	// Declare Array to hold up to 20 values for the problem
-	$TreeArray = array(0,1,2,3,4,5,6,7,8,9,10);	
+	$InputArray = array(0,1,2,3,4,5,6,7,8,9,10);	
 
 	// Open Input File
 	$InputFileHandle = fopen("php://stdin","r") or die ("Unable to open Input file!");
 
 	// Get first Record - The first record is the Number of records in the file to process
 	$TestCases = fgets($InputFileHandle);
-	$TreeArray[0] = $TestCases; 
+	$InputArray[0] = $TestCases; 
 
 	// Get the rest of the records
 	$ppp=1;
 	while(!feof($InputFileHandle)) {
-		$TreeArray[$ppp] = fgets($InputFileHandle);
+		$InputArray[$ppp] = fgets($InputFileHandle);
 		$ppp+=1;
 	};	//End While
 
@@ -58,7 +63,7 @@ function GrowTree($BeginningHeight, $Years) {
 	
 	// Main processing Loop, this is where we loop thru the records and solve each one
 	for ($iii=1;$iii<=$RecordsInFile;$iii++){
-		$TreeHeight = (GrowTree($TreeArray[$iii])) . "\n";
+		$TreeHeight = (GrowTree($InputArray[$iii])) . "\n";
 		fwrite($OutPutFileHandle, $TreeHeight);
 	};	//End For
 
