7d182df89e3e0e3adfc0f0f30aa8d5fb5fbb9264
petersparker <rp-github-2k14@ranger-pete.com>
2014-08-21 00:00:10 -0700

    File Handler Code

--- www.hacker-challenge.dev/File Handler Code/File Handler Code.php
+++ www.hacker-challenge.dev/File Handler Code/File Handler Code.php
@@ -1,4 +1,3 @@
-
 <?php
 
 //	Hackerank File Handlers
@@ -6,26 +5,41 @@
 //  One is that they use the first entry to set up information about what is contained in the rest of the file.
 // 	Rather than write statis arrays for each program, this program is designed to dynamically determine what needs to be buit on the fly, and build it.
 // 	It will use some constants 
-//
-//	Submission by Peter S. Parker
-//	Date: 2014-08- 
-//	Written 2014-08-18 -> 19 in PHP
-//	http://github.com/petersparker/
-//	License: Share and Share Alike, but under no circumstances may this be presented as your own work
-//	especialy to any Hacker Challenges
-//
-//
-//  This program is my answer to the Hacker Challenge "Utopian Tree"
-//  The full details of this Hacker Challenge are at:
-//
-// 		https://www.hackerrank.com/challenges/utopian-tree
-//
-//  Notes to the approach:
-//
-// 
-//
-//  BUGS: 
-//
+//  
+
+
+
+// Declare Globals
+
+$GLOBALS['variable'] = something;
+$GLOBALS['variable'] = something;
+$GLOBALS['variable'] = something;
+$GLOBALS['variable'] = something;
+
+$_2_Dimensional_Array = 
+
+function ParseAStringIntoAnArray($StringToParse, $Delimiter) {
+
+	// Set Horizontal Array Counter
+	$iii = 0;
+	//While != End of String
+	While () {
+		$jjj = 0;
+		// get chars until delimiter
+		// reset string counter
+		$aaa = "";
+		While ($StringToParse != EOString) {
+			if (GetnextChar) != $Delimiter {
+				$aaa = $aaa . $NextChar;
+			};
+		};
+		// put into array element [$iii]
+		$ArrayLine[$iii] = $aaa;	
+		// increment array counter
+		$iii++;
+	};
+	return $ArrayLine;
+};
 
 function GetInputFileAndBuildArrays($A) {
 	$GLOBALS['variable'] = something;
@@ -35,7 +49,12 @@ function GetInputFileAndBuildArrays($A) {
 
 	// Get first Record - The first record is the Number of records in the file to process
 	$TestCases = fgets($InputFileHandle);
+	
+
+
 	$InputArray[0] = $TestCases; 
+
+
 
 	// Get the rest of the records
 	// Redo this to Respect the given variable as well as EOF	
@@ -54,13 +73,17 @@ function GetInputFileAndBuildArrays($A) {
 	return;
 };
 
+
+
 function WorkHorse() {
 
 //	Main Loop - This is where the work gets done
 
+
+
 };
 
-function PutOutputFile($b) {
+function WriteOutputFile($b) {
 	$GLOBALS['variable'] = something;
 	// Open a file for writing
 	$OutPutFileHandle = fopen("php://stdout","w") or die ("Unable to open Output file!");
@@ -84,8 +107,8 @@ function PutOutputFile($b) {
 	// Do the Work
 	WorkHorse();	
 
-	// Write the Answers to the Otput File
-	PutOutputFile($b);
+	// Write the Answers to the Output File
+	WriteOutputFile($b);
 
 // End Main
 
