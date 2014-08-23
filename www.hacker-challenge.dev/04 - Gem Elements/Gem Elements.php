c7a67024f3fe9981dbd6e9ebc8563cf1f887fbeb
petersparker <rp-github-2k14@ranger-pete.com>
2014-08-22 22:29:25 -0700

    I/O Parsing and text cleanup done

--- www.hacker-challenge.dev/04 - Gem Elements/Gem Elements.php
+++ www.hacker-challenge.dev/04 - Gem Elements/Gem Elements.php
@@ -3,10 +3,10 @@
 <html lang="en">
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8">
-	<title>Service Lane PHP</title> 
+	<title>Gem Elements PHP</title> 
 </head>
 <body>
-<h2>Service Lane PHP</h2>
+<h2>Gem Elements PHP</h2>
 <?php
 
 //	Hackerank File Handlers
@@ -17,13 +17,9 @@
 //  
 // Given to me:
 
-//	$LengthOfFreeway 		= Length of Freeway
-//	$TestCases 				= Number of Test Cases - How many time we gonna do this?
+//	$TestCases 				= Number of Records
 
-//	$ServiceLaneArray 		= Array [1..$N] Integers indicating the width of the freeway at each point.
-
-//	$EntryPoint				= Entry Point
-//	$eXitPoint				= Exit Point
+//	$ElementArray 			= Rocks composed of "Elements".
 
 // Declare Globals
 
@@ -37,7 +33,7 @@
 	// Since newlines vary from place to place, and sometimes require \n, <br>, or both, set up a global constant 
 	// by default, constants are global
 		define("aNewLine", "<br> \n");
-		define("TestDebug", "Off", TRUE);
+		define("TestDebug", "ON", TRUE);
 		define("FilesAreLocal", "LOCAL", TRUE);
 	// Set File Locations & Paths
 		if (FilesAreLocal == "LOCAL") {
@@ -60,127 +56,80 @@
 
 // Define the Array
 
-	$ServiceLaneArray = array();
-	$InputArray = array();
-	$TemplineArray = array();
+	$ElementArray = array();
 
 // functions go here
 
-	function WalkThisLane($ServiceLaneArray, $EntryPoint, $eXitPoint) {
-		
-		$LowestValue = 3;
-
-	  	for ($iii=$EntryPoint ; $iii <= $eXitPoint ; $iii++) {
-	 		if  ($ServiceLaneArray[$iii] < $LowestValue) {
-				$LowestValue = $ServiceLaneArray[$iii];
-		 		};
-		};
-		return $LowestValue;
-	};
 
 //  Main
 
-	// Open a file for writing
-	$OutPutFileHandle = fopen($OutputFilePath,'w') or die ("Unable to open Output file!");
-
-	// Reset the Output File to the Beginning, else an extra Linefeed SOMETIMES gets injected.
-	fseek($OutPutFileHandle, 0);
-	
 	// Open Input File
 	$InPutFileHandle = fopen($InputFilePath,'r') or die ("Unable to open Input file!");
 
 	// Get first Record - The first record contains the Length of the Freeway and the 
 	// Number of Test Cases in the file to process
 	
-	$FirstLine = fgets($InPutFileHandle);
-	$TemplineArray = explode(" ", $FirstLine); 
-	$InputArray = $TemplineArray;
+	$RocksToCheck = intval(fgets($InPutFileHandle));
+
+	if (TestDebug == "ON") {
+		echo "RocksToCheck = '" . $RocksToCheck . "'" . aNewLine;
+	};	// Test and Debug
+	
 
 	// Do any analysis needed of the first line here. Often times HackerRank
 	// wants you to take a value on the first line and use it for the number of records to process.
-
-	// echo $InputArray[1];
-
-	$LengthOfFreeway = intval($InputArray[0]);
-	$TestCases = intval($InputArray[1]);
-	
-	// Test and Debug
-	if (TestDebug == "ON") {
-		echo "FirstLine = ";
-		foreach ($TemplineArray as $value) {
-			echo " '" . $value . "' ";
-		};
-		echo aNewLine . "LengthOfFreeway = '" . $LengthOfFreeway . "'". aNewLine;
-		echo "TestCases = '" . $TestCases . "'" . aNewLine;
-		
-	};
-
-	// Get 2nd record - The Service Lane width Data String
-
-	$SecondLine = fgets($InPutFileHandle);
-	$TemplineArray = explode(' ', $SecondLine);
-	
-	// Test and Debug
-	if (TestDebug == "ON") {
-		echo "SecondLine = '" . $SecondLine . "'" . aNewLine;
-	};
-
-	foreach ($TemplineArray as $value) {
-		$value = intval($value);
-		// If Debug ON
-		if (TestDebug == "ON") {
-			echo "'" . $value . "'   ";
-		}
-	};
-	
-	// If Debug ON
-		if (TestDebug == "ON") {
-			echo aNewLine;
-		}
-	
-
-	$ServiceLaneArray = $TemplineArray;
 	
 	// Make this work both ways - EOF and # of Records as per step 1
 	// ## Add to this to Respect the given variable as well as EOF ## //
-for ($iii = 1 ; $iii <= $TestCases ; $iii++) { 
+	for ($iii = 1 ; $iii <= $RocksToCheck ; $iii++) { 
 
-//	while(!feof($InPutFileHandle)) {
-
-		$Answer = Null;
-	
-		$NextLine = fgets($InPutFileHandle);
-		$GetEntryExit = explode(" ", $NextLine); 
-		// Clean up the input
-		$EntryPoint = intval($GetEntryExit[0]);
-		$eXitPoint = intval($GetEntryExit[1]);
-		
-		if (TestDebug == "ON") {
-				echo "Answer Reset = '" . $Answer . "', Entry = '" . $EntryPoint . "', eXit = '" . $eXitPoint . "'  ";
-			};
-
-		// Call data handler HERE
-		$Answer = intval(WalkThisLane($ServiceLaneArray, $EntryPoint, $eXitPoint));
+		$Element = fgets($InPutFileHandle);
 
 		if (TestDebug == "ON") {
-			echo "Answer = '" . $Answer . "'" . aNewLine;
-		};	// Test and Debug
+			echo "Element Before = '" . $Element . "'" . aNewLine;
+		};
+		// Clean up the input - Strip leading and trailing chars
 
-		// Add a linefeed to $Answer
-//		$Answer .= "\n";
+		$Element = preg_replace("/[^a-zA-Z]/", "", $Element);
 
-		// Write the line
-		fwrite($OutPutFileHandle, $Answer);
-		fwrite($OutPutFileHandle, "\n");
+		$ElementArray[$iii-1] = $Element;
 
+		if (TestDebug == "ON") {
+			echo "Element After = '" . $Element . "'" . aNewLine;
 
-	};	//End While
+		};
+
+	}; 	// End For	
+
+		if (TestDebug == "ON") {
+			var_dump($ElementArray);
+		};
+
+	$Answer = Null;
+
+	// Call data handler HERE
+	$Answer = intval("222");
+
+	if (TestDebug == "ON") {
+		echo "Answer = '" . $Answer . "'" . aNewLine;
+	};	// Test and Debug
+
+	// Open a file for writing
+	$OutPutFileHandle = fopen($OutputFilePath,'w') or die ("Unable to open Output file!");
+
+	// Reset the Output File to the Beginning, else an extra Linefeed SOMETIMES gets injected.
+	fseek($OutPutFileHandle, 0);
+
+	// Write the line
+	fwrite($OutPutFileHandle, $Answer);
+	fwrite($OutPutFileHandle, "\n");
 
 	// Close Files
 	fclose($InPutFileHandle);
 	fclose($OutPutFileHandle);
 
 // End Main
+
 
 ?>
 
