209f13aea8972e4f70d0fb112b2ed3f1184c7249
petersparker <rp-github-2k14@ranger-pete.com>
2014-09-09 23:50:48 -0700

    ReOrganize and Manessa

--- www.hackerrank-challenges.dev/10 - Manasa and Stones/Manasa and Stones.php
+++ www.hackerrank-challenges.dev/10 - Manasa and Stones/Manasa and Stones.php
@@ -34,7 +34,7 @@
 		define("aNewLine", "<br> \n");
 		define("TestDebug", "ON", TRUE);	// "ON" for TestDebug
 		define("FilesAreLocal", "LOCAL", TRUE);	// "LOCAL" for Local Files
-		define("ShowOutput", "YES", TRUE);	// "YES" Just Show Answers
+		define("ShowOutput", "ON", TRUE);	// "YES" Just Show Answers
 	// Set File Locations & Paths as constants / globals
 		if (FilesAreLocal == "LOCAL") {
 			define("InputFilePath", 'LocalInputFile.txt');
@@ -58,10 +58,11 @@
 
 // functions go here
 
-	function EchoBug($A="", $B, $C) {
+	function EchoBug($A="", $B, $C=1, $D=0) {
 		// $A is the name of the variable
 		// $B is the variable or array variable
 		// $C is an integer for how many newlines to add for formatting / prettying up
+		// $D is an optional force on (Show Answer)
 
 		if (TestDebug == "ON") {
 			
@@ -73,7 +74,6 @@
 				echo "$A = '" . $B . "'";
 				echo ($C == 1 ? aNewLine : ", ");
 			};
-
 
 			if ($C >=1) {
 					for ($iii=1 ; $iii <= $C ; $iii++ ) { 
@@ -89,7 +89,7 @@
 		// $B is the variable or array variable
 		// $C is a boolean: 0 (Zero) for FALSE, 1 for (True) - TRUE to write a newline
 
-		if (ShowOutput == "YES") {
+		if (ShowOutput == "ON") {
 			
 			if (is_array($B) == TRUE) {
 				echo "$A = '" . aNewLine;
@@ -110,19 +110,28 @@
 
 //  Main
 
-	xdebug_break();
+	// Houston, We are go for Launch...
 
+		EchoBug("Begin", "Blasting Off", 1);
+
+	// xdebug_break();
+	
+
+	// Declare globals
+		$StonesArray = NULL;
+		$StonesArray = Array();
+	
 	// Open Files
-	$InPutFileHandle = fopen(InputFilePath,'r') or die ("Unable to open Input file!");
-	$OutPutFileHandle = fopen(OutputFilePath,'w') or die ("Unable to open Output file!");
+		$InPutFileHandle = fopen(InputFilePath,'r') or die ("Unable to open Input file!");
+		$OutPutFileHandle = fopen(OutputFilePath,'w') or die ("Unable to open Output file!");
 	// Reset the Output File to the Beginning, else an extra Linefeed gets injected.
 		fseek($OutPutFileHandle, 0);
 
 	//	Get first Record - The first record contains the Number of Test Cases
 	//	in the file to process
 	//  Use intval to strip off trailing "\n"
-	$Testcases = intval(fgets($InPutFileHandle));
-	EchoBug("Testcases", $Testcases, 1);
+		$Testcases = intval(fgets($InPutFileHandle));
+		EchoBug("Testcases", $Testcases, 1);
 	
 	// Do any analysis needed of the first line here.
 	
@@ -130,7 +139,7 @@
 	// ## Add to this to Respect the given variable as well as EOF ## //
 	for ($iii = 1 ; $iii <= $Testcases ; $iii++) { 
 
-		//	Read three Records
+		//	Read three Records, convert to Integer to strip linefeeds "\n"
 
 			$n_NumStones = intval(fgets($InPutFileHandle));
 			EchoBug("n_NumStones", $n_NumStones, 0);
@@ -143,22 +152,39 @@
 
 		// Do the Stepping Stone Steps
 			$FirstStone = 0;
-			$Maxkkk = 4; // this is arbitrary
-			$StonesArray = array(25, 20, 456, 43782,324,564,25, 20, 456, 43782,324,564,1,234,8,7546,212, "Dummy Data");
-			
 
-//			for ($jjj=0; $jjj <= $n_NumStones; $jjj++) { 
-				// 
-//				for ($kkk=0; $kkk=$Maxkkk ; $kkk++) { 
-					//
-//				};	//	end for $kkk
-			//	array_push($StonesArray, $kkk);
-//			};	//	end for $jjj
+		//  Set up the loopWalker boundaries
+			// The height of the array is 2 ^ (Stones - 1)
+			$Maxjjj = pow(2,($n_NumStones - 1)); // Adjust for arrays starting with zero instead of one
+			EchoBug("Maxjjj", $Maxjjj, 1);			
+			// The Width of the array is the number of stones -1 to adjust for the fact that arrays start with zero
+			$Maxkkk = $n_NumStones - 1; 
+			EchoBug("Maxkkk", $Maxkkk, 1);			
+			
+			// Reset the width walker Array to empty
+			$LastArray = NULL;
+			$LastArray = array();
+			EchoBug("LastArray", $LastArray, 1); // Should be empty
+
+			for ($jjj=0; $jjj <= $Maxjjj; $jjj++) { 
+				EchoBug("Outer Loop jjj", $jjj, 1);
+				for ($kkk=0; $kkk<=$Maxkkk ; $kkk++) { 
+						EchoBug("Coefficient of A (kkk)", $kkk, 1);
+						EchoBug("Coefficient of B (n_NumStones - kkk)", ($n_NumStones - $kkk), 1);
+						array_push($LastArray, ($a_Diff * $kkk) + ($b_Diff * ($Maxkkk - $kkk)));
+						EchoBug("Last Array", $LastArray, 1);
+				};	//	end for $kkk
+				sort($LastArray);
+				array_unique($LastArray);
+				array_push($StonesArray, end($LastArray));
+				EchoBug("Stones Array", $StonesArray, 1);
+			};	//	end for $jjj
 	}; 	// End For $TestCases
 
 	//	Write Final Answer
 
-		$StonesArray = array(25, 20, 456, 43782,324,564,25, 20, 456, 43782,324,564,1,234,8,7546,212);
+// Dummy data for testing sort and unique
+//		$StonesArray = $FinalAnswer;
 		EchoBug("Stones Array", $StonesArray, 1);
 
 		$StonesArray = array_unique($StonesArray);
